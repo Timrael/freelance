8e7638edc7e82890afc60b65eea8703c5117acac
Timur Kozmenko <timraell@gmail.com>
2013-02-21 17:16:57 +1100

    Introduce create validation Bid#project_cannot_have_chosen_bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -10,6 +10,7 @@ class Bid < ActiveRecord::Base
   validates_numericality_of :price, :greater_than_or_equal_to => 0
   validates :chosen, :inclusion => {:in => [true, false]}
   validate :project_author_cannot_be_bid_author, :chosen_bid_cannot_be_more_than_one
+  validate :project_cannot_have_chosen_bid, :on => :create
 
   before_validation :set_default_attributes
   before_update :launch_project, :if => :have_been_chosen?
@@ -29,6 +30,11 @@ class Bid < ActiveRecord::Base
   def chosen_bid_cannot_be_more_than_one
     errors.add(:chosen, "can't be more than one per project") if
         self.project.present? && self.have_been_chosen? && self.project.have_chosen_bid?
+  end
+
+  def project_cannot_have_chosen_bid
+    errors.add(:project_id, "has chosen bid. You can't add new one.") if
+        self.project.present? && self.project.have_chosen_bid?
   end
 
   def set_default_attributes
